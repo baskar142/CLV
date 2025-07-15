f74f16d430c6b7b2c3ee9aa6b68f6168a7508177
baskar142 <baskarpmkevin@gmail.com>
2025-07-15 16:36:35 +0530

    data validation completed

--- CLV_Lux/pipline/training_pipeline.py
+++ CLV_Lux/pipline/training_pipeline.py
@@ -3,12 +3,19 @@ from CLV_Lux.exception import CLVException
 from CLV_Lux.logger import logging
 
 from CLV_Lux.components.data_ingestion import DataIngestion
-from CLV_Lux.entity.config_entity import DataIngestionConfig
-from CLV_Lux.entity.artifact_entity import DataIngestionArtifact
+from CLV_Lux.components.data_validation import DataValidation
+
+
+from CLV_Lux.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+
+
+from CLV_Lux.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact)
+
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -28,6 +35,30 @@ class TrainPipeline:
 
         except Exception as e:
             raise CLVException(e, sys) from e
+        
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise CLVException(e, sys) from e
 
     def run_pipeline(self) -> None:
         """
@@ -35,7 +66,8 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # Next steps like data transformation, model training, etc. go here...
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            
 
         except Exception as e:
             raise CLVException(e, sys) from e
